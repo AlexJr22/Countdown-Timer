62d10336d77dbf7ea9b3442cbaaaa00c95cfbe03
Alex Junior <alexjr2oficial@gmail.com>
2023-01-04 19:14:59 -0300

    atualizando getTimer.js

--- script/getTimes.js
+++ script/getTimes.js
@@ -1,7 +1,7 @@
 
 const timer = {
-  dataAtual: new Date(),                  // pegando o tempo atual
-  dataFutura: new Date('11, 12, 2023'),   // pegando o tempo de uma data futura
+  dataAtual: new Date(),                      // pegando o tempo atual
+  dataFutura: new Date('7, 9, 2023 18:56'),   // pegando o tempo de uma data futura
 
   Segundos: () => {
     const segundos = timer.dataFutura.getTime() - timer.dataAtual.getTime();
