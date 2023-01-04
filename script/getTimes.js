b56ded45e5ea34747c25182e321ab1298d4d97b9
Alex Junior <alexjr2oficial@gmail.com>
2023-01-04 10:02:09 -0300

    criando uma função que calcula os minutos

--- script/getTimes.js
+++ script/getTimes.js
@@ -3,15 +3,10 @@ const timer = {
   dataAtual: new Date(),                  // pegando o tempo atual
   dataFutura: new Date('11, 12, 2023'),   // pegando o tempo de uma data futura
 
-  secunds: 1000,                          // segundo em milisegundos
-  minute: 1000 * 60,                      // minuto em milisecundos
-  hour: (1000 * 60) * 60,                 // hora em milisegundos
-  day: ((1000 * 60) * 60) * 24,           // day em milisegundos
-
   Segundos: () => {
     const segundos = timer.dataFutura.getTime() - timer.dataAtual.getTime();
 
-    return Math.round(segundos / timer.secunds);
+    return Math.round(segundos / 1000);
   }
 
 };
--- script/render.js
+++ script/render.js
@@ -3,18 +3,25 @@ import { relogio } from "./relogio.js";
 // elementos que exibirão o tempo
 // const Dia = document.getElementById('Dia');
 // const Hora = document.getElementById('Hora');
-// const Minuto = document.getElementById('Minuto');
+const Minuto = document.getElementById('Minuto');
 const Segundo = document.getElementById('Segundo');
 
 const render = {
 
   tempoEmSegundos: tempo => tempo % 60,
-  tempoEmMinutos: tempo => Math.floor(tempo / 60),
+  tempoEmMinutos: tempo => Math.floor(tempo / 60) % 60,
   
   render() {
+
     Segundo.innerHTML = `
       ${String(render.tempoEmSegundos(relogio.tempo)).padStart(2, "0")}
     `;
+    
+    Minuto.innerHTML = `
+      ${String(render.tempoEmMinutos(relogio.tempo)).padStart(2, "0")}
+    `;
+  
+    
   },
   
 }
