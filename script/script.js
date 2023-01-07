f3f55fa90fbb1cdef834a130c274f290afbd5529
Alex Junior <alexjr2oficial@gmail.com>
2023-01-07 11:47:11 -0300

    refatorando o script.js

--- script/script.js
+++ script/script.js
@@ -1,5 +1,16 @@
 import { relogio } from "./relogio.js";
 
-const btnIniciar = document.getElementById('BtnIniciar');
+const input = document.getElementById('inputData');                             
+let dataFutura = new Date;
 
-btnIniciar.addEventListener('click', () => relogio.iniciar());
+input.addEventListener('change', () => {
+
+  const valueInput = String(input.value).split('-');                            // converte o valor do input para um array com 3 posições
+
+  dataFutura = new Date(`${valueInput[1]} ${valueInput[2]} ${valueInput[0]}`);  // retornar a data futura para ser usada no getTimes.js
+  console.log(dataFutura);
+});
+
+const btnIniciar = document.getElementById('BtnIniciar');                       // botão calcular
+
+btnIniciar.addEventListener('click', () => relogio.iniciar(dataFutura));        // inicia a contagem regressiva
