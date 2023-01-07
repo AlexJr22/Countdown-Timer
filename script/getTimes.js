78028c1d1d9ede604aebab4fb42cf61249475d2a
Alex Junior <alexjr2oficial@gmail.com>
2023-01-07 11:45:56 -0300

    refatorando o gettimes.js

--- script/getTimes.js
+++ script/getTimes.js
@@ -1,18 +1,7 @@
-const input = document.getElementById('inputData');
-let dataFutura = new Date;
-
-input.addEventListener('change', () => {
-  
-  const valueInput = String(input.value).split('-');
-
-  dataFutura = new Date(`${valueInput[1]} ${valueInput[2]} ${valueInput[0]}`);  // pegando o tempo de uma data futura
-  console.log(dataFutura)
-});
-
 const timer = {
   dataAtual: new Date(),                      // pegando o tempo atual
 
-  Segundos: () => {
+  Segundos: (dataFutura) => {
     const segundos = dataFutura.getTime() - timer.dataAtual.getTime();
 
     return Math.round(segundos / 1000);
