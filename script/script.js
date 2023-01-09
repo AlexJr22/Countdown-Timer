71758779a338fbbabc7155c4281697507efaa653
Alex Junior <alexjr2oficial@gmail.com>
2023-01-09 11:19:17 -0300

    adicionando uma função pro novo botão

--- script/script.js
+++ script/script.js
@@ -1,16 +1,26 @@
 import { relogio } from "./relogio.js";
 
-const input = document.getElementById('inputData');                             
+const input = document.getElementById('inputData');                             // pegando o input=date
+const btnIniciar = document.getElementById('BtnIniciar');                       // botão calcular
+const btnNovaData = document.getElementById('btnNovaData');                     // botão para escolher uma nova data
+
 let dataFutura = new Date;
 
 input.addEventListener('change', () => {
-
+  
   const valueInput = String(input.value).split('-');                            // converte o valor do input para um array com 3 posições
-
+  
   dataFutura = new Date(`${valueInput[1]} ${valueInput[2]} ${valueInput[0]}`);  // retornar a data futura para ser usada no getTimes.js
   console.log(dataFutura);
 });
 
-const btnIniciar = document.getElementById('BtnIniciar');                       // botão calcular
-
 btnIniciar.addEventListener('click', () => relogio.iniciar(dataFutura));        // inicia a contagem regressiva
+
+btnNovaData.addEventListener('click', () => {                                   // encerra a contagem regrassiva para que o usuário escolha uma nova data
+  
+  clearInterval(relogio.interval);
+  relogio.tempoAtual = relogio.tempoInicial;
+
+  document.getElementById('containerData').style.display = 'flex';
+  document.getElementById('containerTempo').style.display = 'none';
+});
